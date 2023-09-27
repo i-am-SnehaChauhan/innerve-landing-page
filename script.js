08ca9f0dc91cc0933f7e3ba5db12e33493eb1543
SNEHA CHAUHAN <sneha2003chauhan@gmail.com>
2023-09-27 16:47:40 +0000

    Preloader Added

--- script.js
+++ script.js
@@ -1,102 +1,103 @@
+// Include GSAP and ScrollTrigger
 gsap.registerPlugin(ScrollTrigger);
 
+// Landing Page Animations
 ScrollTrigger.defaults({
-  toggleActions:"play none none reverse"
-})
+  toggleActions: "play none none reverse"
+});
 
-
-gsap.to('.img-container',{
-  scale:52,
-  ease:"ease",
-  scrollTrigger:{
-    trigger:'.video-section',
-    scrub:1,
-    start:"top top",
-    end:"bottom",
-    pin:true
+gsap.to('.img-container', {
+  scale: 52,
+  ease: "ease",
+  scrollTrigger: {
+    trigger: '.video-section',
+    scrub: 1,
+    start: "top top",
+    end: "bottom",
+    pin: true
   }
-})
+});
 
-
-gsap.to('.right' ,{
-  autoAlpha:0,
-  x:500,
-  duration:1.5,
-  scrollTrigger:{
-    start:1
+gsap.to('.right', {
+  autoAlpha: 0,
+  x: 500,
+  duration: 1.5,
+  scrollTrigger: {
+    start: 1
   }
-})
-gsap.to('.left' ,{
-  autoAlpha:0,
-  x:300,
-  duration:1.5,
-  scrollTrigger:{
-    start:1
+});
+
+gsap.to('.left', {
+  autoAlpha: 0,
+  x: 300,
+  duration: 1.5,
+  scrollTrigger: {
+    start: 1
   }
-})
+});
 
-
-gsap.to('.txt-bottom',{
-  autoAlpha:0,
-  letterSpacing:-10,
-  duration:2,
-  scrollTrigger:{
-    start:2
+gsap.to('.txt-bottom', {
+  autoAlpha: 0,
+  letterSpacing: -10,
+  duration: 2,
+  scrollTrigger: {
+    start: 2
   }
-})
-
+});
 
 const tl = gsap.timeline();
-
-tl.from('.left-side div',{
-  y:100,
-  opacity:0,
-  stagger:{
-    amount:0.2
-  }
+tl.from('.left-side div', {
+    y: 100,
+    opacity: 0,
+    stagger: {
+        amount: 0.2
+    }
 })
-.from('.right-side',{opacity:0,duration:2},.5)
-// .to('.wrapper' ,{x:-window.innerWidth})
-
-
+.from('.right-side', {
+    opacity: 0,
+    duration: 2
+}, 0.5);
 
 ScrollTrigger.create({
-  animation:tl,
-  trigger:'.wrapper',
-  start:"top top",
-  end:"+=600",
-  scrub:1,
-  pin:true,
-  ease:"ease"
-})
-
-
-
-// gsap.utils.toArray('.col').forEach(image=>{
-//   gsap.fromTo(image,{
-//     opacity:.3,
-//     x:0
-//   },{
-//     opacity:1,
-//     x:-50,
-//     scrollTrigger:{
-//       trigger:image,
-//       start:"10%",
-//       stagger:{
-//         amount:.4
-//       }
-//     }
-//   })
-// })
+  animation: tl,
+  trigger: '.wrapper',
+  start: "top top",
+  end: "+=600",
+  scrub: 1,
+  pin: true,
+  ease: "ease"
+});
 
 const timeline = gsap.timeline();
-
-timeline.from('.title span' ,{
-  y:150,
-  skewY:7,
-  duration:3
-}).from('.txt-bottom',{
-  letterSpacing:-10,
-  opacity:0,
-  duration:3
+timeline.from('.title span', {
+    y: 150,
+    skewY: 7,
+    duration: 3
 })
+.from('.txt-bottom', {
+    letterSpacing: -10,
+    opacity: 0,
+    duration: 3
+});
+
+// Preloader Script
+$(document).ready(function () {
+  var $scramble = $(".scramble");
+  $scramble.scramble(3000, 20, "alphabet", true);
+
+  // Loader Animation
+  const loaderTimeline = gsap.timeline();
+  loaderTimeline.to(".loading", {
+    opacity: 0,
+    delay: 2.5,
+  })
+  .to(".loader", {
+    opacity: 0,
+    duration: 1,
+    ease: "expo.easeinout",
+  })
+  .to(".loader", {
+    y: "-100%",
+    duration: 1,
+  });
+});
